e7b2ab740a8c7275eef0a134d950d9669876ab18
leekun <leekun030.163.com>
2018-01-16 14:31:25 +0800

    无

--- src/modules/wxapi/wxappapi.service.ts
+++ src/modules/wxapi/wxappapi.service.ts
@@ -5,15 +5,13 @@ import { Component, Query, Response} from '@nestjs/common';
 export class WxappApiService {
     private readonly url = "https://api.weixin.qq.com/sns/jscode2session?appid=wxe0fdae328482d4ba&secret=6a9ce90d3205537185687ccca3371440&js_code=JSCODE&grant_type=authorization_code";
 
-    private data = {};
-
     setUserSessionKey(@Query() query, @Response() res) {
         if(query.code.length<=0) {
             res.end();
         }
 
-        this.data = res.location(this.url);
-        console.log(this.data);
+       
+        
 
     }
 }
